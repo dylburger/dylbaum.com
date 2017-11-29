83cd0c6580bdea56a44a0a3a258de9a0cfd5ceaa
Dylan Sather <dylan.sather@gmail.com>
2017-11-29 01:21:32 -0500

    Adding config.js file to .gitignore, adding config template

---
+++ src/utils/config.template.js
@@ -0,0 +1,13 @@
+const config = {
+  venue: {
+    latitude: 100,
+    longitude: -100,
+    mapZoomLevel: 12,
+  },
+  mapbox: {
+    accessToken:
+      'token',
+  },
+};
+
+export default config;
