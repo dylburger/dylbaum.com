d2f35137b34bf73e0ec0b2204ac2548735049e09
Dylan Sather <dylan.sather@gmail.com>
2017-11-29 20:57:51 -0500

    Adding hotel info

--- src/pages/hotels.js
+++ src/pages/hotels.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import config from '../utils/config';
+import PageTitle from './PageTitle';
 
 const Hotels = () => (
   <div>
-    <h1>Hotels</h1>
+    <PageTitle title="Hotels" />
+    <p>Hotel information coming soon!</p>
+    <p>
+      If you&rsquo;re planning to stay in the area before or after the wedding,{' '}
+      <a href={config.surroundingArea.airbnbLink}>Airbnb</a> is a great option!
+    </p>
   </div>
 );
 
