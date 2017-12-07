6cbeb087876d179bdf670a8a1d20217f41eb5765
Dylan Sather <dylan.sather@gmail.com>
2017-12-06 23:05:23 -0800

    Adding RSVP

---
+++ src/pages/rsvp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import config from '../utils/config';
+import PageTitle from './PageTitle';
+
+const RSVP = () => (
+  <div>
+    <PageTitle title="RSVP" />
+    {config.rsvp.formHTML}
+  </div>
+);
+
+export default RSVP;
