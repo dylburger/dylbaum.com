6743d6b9b801a11310bbfd901adc0d5e9064d046
Dylan Sather <dylan.sather@gmail.com>
2017-12-24 18:13:51 -0700

    Adding sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   siteMetadata: {
     title: `Wedding Website`,
+    siteUrl: 'https://dylbaum.com',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+  ],
+};
