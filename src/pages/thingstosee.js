7bfe9ab2a6dd01849e9cd7e22f0870ed90f82114
Dylan Sather <dylan.sather@gmail.com>
2018-06-11 17:14:35 -0700

    Adding Things to see component

---
+++ src/pages/thingstosee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import config from '../utils/config';
+import PageTitle from '../components/PageTitle';
+
+export default () => (
+  <div>
+    <PageTitle title="Things to See" />
+  </div>
+);
