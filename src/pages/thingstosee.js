5b27262e992eb69d5de7ba523ecd589b962d54b3
Dylan Sather <dylan.sather@gmail.com>
2018-06-12 15:01:04 -0700

    Modifying functions in Things to see component

--- src/pages/thingstosee.js
+++ src/pages/thingstosee.js
@@ -4,8 +4,26 @@ import Link from 'gatsby-link';
 import config from '../utils/config';
 import PageTitle from '../components/PageTitle';
 
+const generateActivitiesDescription = arrayOfActivities => (
+  <div>
+    {config.surroundingArea.introText}
+    <ul
+      style={{
+        listStyle: 'none',
+      }}>
+      {generateListOfActivities(arrayOfActivities)}
+    </ul>
+  </div>
+);
+
+const generateListOfActivities = arrayOfActivities =>
+  arrayOfActivities.map((activity, i) => (
+    <li key={`activity-${i}`}>{activity.text}</li>
+  ));
+
 export default () => (
   <div>
     <PageTitle title="Things to See" />
+    {generateActivitiesDescription(config.surroundingArea.thingsToSee)}
   </div>
 );
